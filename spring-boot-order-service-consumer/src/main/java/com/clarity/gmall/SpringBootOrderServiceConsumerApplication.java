1bc54da96bb93c3e2188cb9c9c33263361d0303f
clarity <2270893459@qq.com>
2022-11-19 14:13:32 +0800

    test spring boot 启动服务消费者远程调用服务

--- spring-boot-order-service-consumer/src/main/java/com/clarity/gmall/SpringBootOrderServiceConsumerApplication.java
+++ spring-boot-order-service-consumer/src/main/java/com/clarity/gmall/SpringBootOrderServiceConsumerApplication.java
@@ -1,11 +1,9 @@
 package com.clarity.gmall;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo
 public class SpringBootOrderServiceConsumerApplication {
 
     public static void main(String[] args) {
